434521ab9d3a6a619e4ba948cd2ab47cc9e3621f
bluesense <a.thirumurugan13943@gmail.com>
2023-01-04 11:27:44 -0500

    linearsearch

---
+++ linearsearch.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+	int a;
+	printf("Enter the length of array  ");
+	scanf("%d",&a);
+
+	int arr[a];
+
+	printf("\nEnter the set of elements  ");
+	for (int i=0;i<a;i++) {
+		scanf("%d",&arr[i]);
+	}
+
+	printf("\nEnter the element to search  ");
+	int se;
+	scanf("%d",&se);
+
+
+	//Linear searching for an element
+
+	for (int i=0;i<a;i++) {
+		if (arr[i]==se) {
+			printf("\nElement is present in %d Index || element =  %d",i,arr[i]);
+		}
+	}
+
+	return 0;
+}
